fa7b93ee45fd62780d0dc259a0eea6d8410f62b9
NoorAzam-1 <azamnoor698@gmail.com>
2026-05-21 17:45:57 +0530

    feat: initialize frontend with Next.js and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Configured PostCSS with Tailwind CSS plugin

---
+++ frontend/next.config.mjs
@@ -0,0 +1,18 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+     domains: ["source.unsplash.com"],
+  },
+};
+
+
+export default nextConfig;
